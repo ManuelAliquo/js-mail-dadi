cf995b03f1e2056a8f7c880dd759e6f5a80529ee
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-19 15:14:02 +0100

    dadi: added random numbers generators

---
+++ dadi/main.js
@@ -0,0 +1,10 @@
+// # Dadi
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Il numero più alto vince.
+
+const playerNumber = Math.floor(Math.random() * 6 + 1);
+console.log(playerNumber);
+
+const pcNumber = Math.floor(Math.random() * 6 + 1);
+console.log(pcNumber);
