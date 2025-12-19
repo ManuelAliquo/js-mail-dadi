289743132b5cbae3760bd10bf02131d8a1827fcb
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-19 15:07:27 +0100

    mail: added for loop and output msg

--- mail/main.js
+++ mail/main.js
@@ -9,5 +9,17 @@ const emailList = ["topolino", "paperino", "pluto"];
 console.table(emailList);
 
 // - Chiedo all'utente la sua email
-let userEmail = prompt("Inserisci la tua email");
-console.log(`User email: "${userEmail}"`);
+const userEmail = prompt("Inserisci la tua email");
+console.log("User email:", userEmail);
+console.log("-------------");
+
+let foundEmail = `"Your email isn't in the list, you got to leave.."`;
+
+for (let i = 0; i < emailList.length; i++) {
+  const currentEmail = emailList[i];
+  isUserEmail = userEmail === currentEmail;
+  if (isUserEmail === true) {
+    foundEmail = `"Your email is in the list, welcome to the party!"`;
+  }
+}
+console.log(foundEmail);
