b9679628c74e704faf4398b7a3ad0af5c2535af5
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-19 15:24:38 +0100

    dadi: added winning condition and output

--- dadi/main.js
+++ dadi/main.js
@@ -3,8 +3,19 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Il numero più alto vince.
 
-const playerNumber = Math.floor(Math.random() * 6 + 1);
-console.log(playerNumber);
+let playerNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Player:", playerNumber);
 
-const pcNumber = Math.floor(Math.random() * 6 + 1);
-console.log(pcNumber);
+let pcNumber = Math.floor(Math.random() * 6 + 1);
+console.log("PC:", pcNumber);
+
+let outputMsg;
+
+if (playerNumber === pcNumber) {
+  outputMsg = `"Numbers are equal"`;
+} else if (playerNumber > pcNumber) {
+  outputMsg = `"Player Wins"`;
+} else if (pcNumber > playerNumber) {
+  outputMsg = `"PC Wins"`;
+}
+console.log(outputMsg);
